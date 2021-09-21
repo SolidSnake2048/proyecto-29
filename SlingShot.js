da50370eefdc3df0471deadbe1a202abf56e4231
SolidSnake2048 <88735858+SolidSnake2048@users.noreply.github.com>
2021-09-21 14:52:34 -0500

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.1,
+            length:10
+        }
+        this.pointB=pointB;
+        this.slingshot=Constraint.create(options);
+        World.add(world,this.slingshot);
+    }
+    fly(){
+        this.slingshot.bodyA=null;
+    }
+    display(){
+        if(this.slingshot.bodyA){
+            var pointA=this.slingshot.bodyA.position;
+            var pointB=this.pointB;
+            fill("black");
+            strokeWeight(10);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+}
